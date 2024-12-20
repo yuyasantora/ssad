c077ce1a86a42aa194ee089b01b13be3b7e1ed08
yuyasantora <uya1202@yahoo.co.jp>
2024-12-20 14:43:10 +0900

    num_classes

--- demo/detection.py
+++ demo/detection.py
@@ -5,13 +5,14 @@ from torchvision.models.detection import fcos_resnet50_fpn
 from demo.image_encoder import ResnetEncoder
 
 class FCOSDetector(nn.Module):
-    def __init__(self, device="cuda"):
+    def __init__(self, device="cuda",num_classes=32):
         super().__init__()
         self.device = device
         self.model = fcos_resnet50_fpn(pretrained=False)
         self.model.to(self.device)
         # バックボーンをImageEncoderと共有
         self.model.backbone = ResnetEncoder(self.device)
+        self.model.head.classification_head.num_classes = num_classes+1
 
     def forward(self, image):
         return self.model(image)
