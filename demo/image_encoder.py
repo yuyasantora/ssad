bb4d9bb9fd39fed086f7d02554ff1b1f1d2ec790
yuyasantora <67677852+yuyasantora@users.noreply.github.com>
2024-12-26 13:46:22 +0900

    Add FPN to Resnet Encoder

--- demo/image_encoder.py
+++ demo/image_encoder.py
@@ -4,17 +4,19 @@ import timm
 import torchvision
 
 class ResnetEncoder(nn.Module):
-    def __init__(self, model_name="resnet50",output_channels=256):
+    def __init__(self, model_name="fcos_resnet50_fpn",output_channels=256):
         super().__init__()
-        self.model = torchvision.models.resnet50(pretrained=True)
-        self.model = nn.Sequential(*list(self.model.children())[:-2])
+        self.model = torchvision.models.detection.fcos_resnet50_fpn(pretrained=False)
+        self.model = self.model.backbone
+        #self.model = nn.Sequential(*list(self.model.children())[:-2])
         self.output_channels = output_channels
         # 1x1畳み込み層でチャネル数を256に変換
         self.conv1x1 = nn.Conv2d(2048, self.output_channels, kernel_size=1)
 
+        
     def forward(self, x):
         x = self.model(x)   
-        x = self.conv1x1(x)
+    
         return x
     
     
