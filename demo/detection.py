df7815a07721da1a702ce0bf23eb50c268507c5e
yuyasantora <67677852+yuyasantora@users.noreply.github.com>
2024-12-27 10:25:37 +0900

    change nub_groups = 8

--- demo/detection.py
+++ demo/detection.py
@@ -6,17 +6,32 @@ from torchvision.models.detection.fcos import FCOSClassificationHead
 from demo.image_encoder import ResnetEncoder
 
 class FCOSDetector(nn.Module):
-    def __init__(self, device="cuda",num_classes=32):
+    def __init__(self, device="cuda",num_classes=32, num_groups=8):
         super().__init__()
+
         self.device = device
         self.model = fcos_resnet50_fpn(pretrained=False)
         self.model.to(self.device)
         # バックボーンをImageEncoderと共有
         self.model.backbone = ResnetEncoder(self.device)
         # 分類ヘッドをクラス数+1に変更
-        num_anchors = self.model.head.classification_head.num_anchors
+        num_anchors = self.model.head.classification_head.num_anchors 
         self.model.head.classification_head.cls_logits = FCOSClassificationHead(in_channels=256, num_classes=num_classes+1, num_anchors=num_anchors)
 
+        # カスタムGroupNormレイヤーの挿入
+        ## num_groupsは8に設定
+        for name, child in self.model.head.classification_head.named_children():
+            if isinstance(child, nn.Sequential):
+                for sub_name, sub_child in child.named_children():
+                    if isinstance(sub_child, nn.GroupNorm):
+                        sub_child.num_groups = 8
+
+        for name, child in self.model.head.regression_head.named_children():
+            if isinstance(child, nn.Sequential):
+                for sub_name, sub_child in child.named_children():
+                    if isinstance(sub_child, nn.GroupNorm):
+                        sub_child.num_groups = 8
+        
     def forward(self, image, target):
         return self.model(image, target)
     
